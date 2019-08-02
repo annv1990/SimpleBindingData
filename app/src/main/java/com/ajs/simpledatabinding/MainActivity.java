7fe0b611b405ae3b4b25b551df310e39612b7a5f
AnNV <an.nguyen@co-well.vn>
2019-08-02 14:11:00 +0700

    - ok

--- app/src/main/java/com/ajs/simpledatabinding/MainActivity.java
+++ app/src/main/java/com/ajs/simpledatabinding/MainActivity.java
@@ -3,6 +3,7 @@ package com.ajs.simpledatabinding;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ajs.simpledatabinding.databinding.ActivityMainBinding;
 
@@ -16,10 +17,16 @@ public class MainActivity extends AppCompatActivity {
         mUser = new User();
         mUser.setName("An Huong");
         mUser.setEmail("annv1990@gmail.com");
+        mUser.setLink("https://api.androidhive.info/images/nature/david1.jpg");
         binding.setUser(mUser);
         User user1 = new User();
         user1.setName("dfdfdf");
         user1.setEmail("ABC, xys dfff");
         binding.content.setUser1(user1);
+    }
+
+    public void onChangeInfoClick(View v){
+        mUser.setName("An Huong 1990");
+        mUser.setEmail("Huongannv1990@gmail.com");
     }
 }
--- app/src/main/java/com/ajs/simpledatabinding/User.java
+++ app/src/main/java/com/ajs/simpledatabinding/User.java
@@ -1,23 +1,54 @@
 package com.ajs.simpledatabinding;
 
-public class User {
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.databinding.BindingAdapter;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+
+public class User extends BaseObservable {
 
     String name;
     String email;
+    String link;
 
+    @Bindable
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(BR.name);
     }
 
+    @Bindable
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+        notifyPropertyChanged(BR.email);
+    }
+
+    @BindingAdapter("android:link")
+    public static void loadImage(ImageView view, String imageUrl) {
+        Glide.with(view.getContext())
+                .load(imageUrl)
+                .into(view);
+
+        // If you consider Picasso, follow the below
+        // Picasso.with(view.getContext()).load(imageUrl).placeholder(R.drawable.placeholder).into(view);
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
     }
 }
